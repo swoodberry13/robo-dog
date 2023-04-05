59401e8e815ac6a757ba6e96afcc99e7183529b5
AdrienAmblard <74068226+AdrienAmblard@users.noreply.github.com>
2023-04-05 13:07:04 -0400

    Leg data Update
    Update leg angles / data since their lengths changed

--- MATLAB Code/SplinePath.m
+++ MATLAB Code/SplinePath.m
@@ -1,10 +1,10 @@
 clear; clc; 
 
-wayPoints = [10.5 -3.5; 10.5 0;...
-    10.5 3.5; 9.5 3;... 
-    9 2; 9.5 0;...
-    9 -2; 9.5 -3;
-    10.5 -3.5]; % XY loc of each waypoint
+wayPoints = [8 -2.5; 8 0;...
+    8 2.5; 7 2;... 
+    6.5 1; 7 0;...
+    6.5 -1; 7 -2;
+    8 -2.5]; % XY loc of each waypoint
 
 
 ts = 1; % time in s between each waypoint
@@ -49,7 +49,7 @@ x = [xx1';xx2';xx3';xx4'];
 y = [yy1';yy2';yy3';yy4'];
 
 % finding theta1 and theta2 at each point using inverse kinematics 
-[theta1 , theta2] = findAngle(x,y,5,7.5); 
+[theta1 , theta2] = findAngle(x,y,5,5); 
 
 %plotting all of the 
 figure 
@@ -76,8 +76,11 @@ theta2 = theta2(s:end);
 theta1 = flip(theta1);
 theta2 = flip(theta2);
 
+theta1 = theta1+90;
+%theta2 = theta2+90;
+
 figure
-legAnimate(theta1,theta2,5,7.5)
+legAnimate(theta1,theta2,5,5)
 
 % this script produces theta1 angles with respect to the x-axis but in
 % reality the it is with respect to the y-axis so
